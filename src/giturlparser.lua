3b043710f5f3101f57bc92b4bf0e8b5e8d31ab96
linrongbin16 <linrongbin16@outlook.com>
2024-01-06 21:13:29 +0800

    chore

--- src/giturlparser.lua
+++ src/giturlparser.lua
@@ -1,3 +1,13 @@
 local giturlparser = {}
 
+--- @class giturlparser.GitUrlInfo
+--- @field protocol string?
+--- @field user string?
+--- @field password string?
+--- @field host string?
+--- @field port string?
+--- @field org string?
+--- @field repo string?
+--- @field path string? e.g. org/repo
+
 return giturlparser
